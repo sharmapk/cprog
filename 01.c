ef31edfc4fac218de847b4c8a5e4fb9fe1f3b6f0
sharmapk05 <pksharma187@gmail.com>
2015-07-27 11:25:29 +0530

    add two no

---
+++ 01.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,add;
+	printf("Enter the value of a and b:");
+//	scanf("a=%d",&a);
+//	scanf("%d",&b);
+a=2,b=3;
+	add=a+b;
+	printf("Addition=%d",add);
+	return 0;
+}
